6d4467f4b939adcd43ebf8321bc92ca66fd68096
Luisotorres3 <74504901+Luisotorres3@users.noreply.github.com>
2024-04-27 01:13:49 +0200

    Update basename

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import "react-tooltip/dist/react-tooltip.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Gestion-del-Olivar">
       <App />
     </BrowserRouter>
   </React.StrictMode>
